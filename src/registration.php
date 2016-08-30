4b0c041c1f0ceead5bd98b4805c1133cf746115c
Ash Smith <ash.smith@meanbee.com>
2016-08-30 08:31:59 +0100

    Fix the typos

--- src/registration.php
+++ src/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Meanbee_ExampleMagento2Module',
+    'Meanbee_VipMembership',
     __DIR__
 );
