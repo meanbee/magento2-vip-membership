851a15b63a4e487e8b73ccaca55a4f4ba8ec0ea1
Ash Smith <ash.smith@meanbee.com>
2016-08-30 09:31:44 +0100

    Create VipMembership product type

---
+++ src/Model/Product/Type/VipMembership.php
@@ -0,0 +1,36 @@
+<?php namespace Meanbee\VipMembership\Model\Product\Type;
+
+class VipMembership extends \Magento\Catalog\Model\Product\Type\AbstractType
+{
+
+    /**
+     * Check is virtual product
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @return bool
+     */
+    public function isVirtual($product)
+    {
+        return true;
+    }
+
+    /**
+     * Check that product of this type has weight
+     *
+     * @return bool
+     */
+    public function hasWeight()
+    {
+        return false;
+    }
+
+    /**
+     * Delete data specific for VipMembership product type
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @return void
+     */
+    public function deleteTypeSpecificData(\Magento\Catalog\Model\Product $product)
+    {
+    }
+}
